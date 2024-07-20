15ca42f5a9a3d3c664f00dd9ce8657ff69e7aa18
Ankush Garg <ankushgarg7123@gmail.com>
2024-07-20 15:28:52 +0530

    Added logic for filtered search and update the user data

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,7 @@ const zod = require("zod");
 const { User } = require("../db");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config");
+const { authMiddleware } = require("../middleware");
 
 const signupSchema = zod.object({
   username: zod.string().email(),
@@ -77,6 +78,52 @@ router.get("/signin", async (req, res) => {
   }
   res.status(411).json({
     message: "Error while logging in",
+  });
+});
+
+const updateSchema = zod.object({
+  firstName: zod.string().optional(),
+  lastName: zod.string().optional(),
+  password: zod.string().optional(),
+});
+router.put("/", authMiddleware, async (req, res) => {
+  const { success } = updateSchema.safeParse(req.body);
+  if (!success) {
+    res.status(411).json({
+      message: "Error while updating information",
+    });
+  }
+  await User.updateOne({ _id: req.userId }, req.body);
+  res.json({
+    message: "Updated successfully",
+  });
+});
+
+router.get("/bulk", async (req, res) => {
+  const filter = req.query.filter || "";
+
+  const users = await User.find({
+    $or: [
+      {
+        firstName: {
+          $regex: filter,
+        },
+      },
+      {
+        lastName: {
+          $regex: filter,
+        },
+      },
+    ],
+  });
+
+  res.json({
+    user: users.map((user) => ({
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
   });
 });
 module.exports = router;
